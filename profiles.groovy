20c743a96f90fd15cb8b6e671968d1d9cd91a66b
Dharmesh Sing <dharmeshsing@gmail.com>
2020-11-04 21:36:40 +1100

    Refactor Code

---
+++ profiles.groovy
@@ -0,0 +1,21 @@
+environments {
+    local {
+        ext.envProp = 'local.properties'
+        ext.softwarePath = '/Users/dharmeshsing/Documents/Masters/tmp'
+    }
+
+    windows {
+        ext.envProp = 'windows.properties'
+        ext.softwarePath = '/Users/dharmeshsing/Documents/cointoss_shared'
+    }
+
+    wits {
+        ext.envProp = 'witsServer.properties'
+        ext.softwarePath = '/hydrahome/1133465/Simulation'
+    }
+
+    chpc {
+        ext.envProp = 'chpcServer.properties'
+        ext.softwarePath = '/home/dsing/Simulation'
+    }
+}
