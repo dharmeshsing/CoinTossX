d629ffff1b50746d447665d8e85afdd3a6d9f6e3
IvanJericevich <ivanjericevich96@gmail.com>
2021-05-04 19:39:34 +0200

    Include example code

---
+++ ClientSimulator/src/main/java/example/Utilities.java
@@ -0,0 +1,36 @@
+package example;
+
+import client.*;
+
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.time.LocalDateTime;
+import com.carrotsearch.hppc.IntObjectMap;
+
+public class Utilities {
+    private static Properties properties = new Properties();
+
+    private static void loadProperties(Properties properties) throws IOException {
+        try (InputStream inputStream = new FileInputStream(Paths.get("").toAbsolutePath().getParent() + "/ClientSimulator/build/install/ClientSimulator/resources/simulation.properties")) {
+            if (inputStream != null) {
+                properties.load(inputStream);
+            } else {
+                throw new IOException("Unable to load properties file simulation.properties");
+            }
+        }
+    }
+
+    public static Client loadClientData(int clientId, int securityId) throws Exception {
+        loadProperties(properties);
+        String dataPath = properties.get("DATA_PATH").toString();
+        IntObjectMap<ClientData> clientData = ClientData.loadClientDataData(dataPath);
+        Client client = new Client(clientData.get(clientId), securityId);
+        client.init(properties);
+        return client;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+}
